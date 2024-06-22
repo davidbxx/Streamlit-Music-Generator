bef404fd7555795f30f05a936bd8a0dad0941176
davidbxx <105551343+davidbxx@users.noreply.github.com>
2024-06-23 00:39:04 +0100

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import numpy as np
+import time
+
+st.title("Music Business Strategy generator Using Machine Language")
+st.markdown("#### **Name: Odunuga Oluwatobiloba**")
+st.markdown("**Matric Number: 20BE027031**")
+st.markdown("**Program: Mass Communication**")
+st.divider()
